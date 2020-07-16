efb1ad4e0a70f0c9b784ac819f79bc27e311bdfe
Huan Carlos <huanc65@gmail.com>
2020-07-15 23:36:06 -0300

    Função que pega os valores dos Inputs

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ function limpar() {
     hex.value = '';
     oct.value = '';
 }
+
+//pega os valores digitados no input
+function pegarValores(){
+    let vet = [4];
+    vet[0] = dec.value;
+    vet[1] = bin.value;
+    vet[2] = hex.value;
+    vet[3] = oct.value;
+    return vet;
+}
