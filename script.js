2857976c841f0c1d573f1937bce0630e199c048c
Huan Carlos <huanc65@gmail.com>
2020-07-15 21:04:45 -0300

    Botão Limpar

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('limpar').onclick = function () {
+    document.getElementById('decimal').value = '';
+    document.getElementById('binario').value = '';
+    document.getElementById('hexadec').value = '';
+    document.getElementById('octal').value = '';
+}
+document.getElementById('converter').onclick = function(){
+    
+}
